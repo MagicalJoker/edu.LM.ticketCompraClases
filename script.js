ce8a6a627d2e09260c474b1e388423ceeeb61620
José Luis Moreno <75699665+MagicalJoker@users.noreply.github.com>
2026-02-21 19:20:31 +0100

    Pasos realizados 5/6
    Por añadir: Persistencia de datos y refactorización (paso extra)

--- script.js
+++ script.js
@@ -19,7 +19,20 @@ class carrito {
         this.listaProductos = [];
     }
     agregarProducto(nombre, precio) {
+        let encontrado = false; 
 
+        for (let i=0; i < this.listaProductos.length; i++) {
+            if (this.listaProductos[i].nombre === nombre) {
+                this.listaProductos[i].aumentarCantidad();
+                encontrado = true;
+                break;
+            }
+        }
+            if (!encontrado) {
+                const nuevoProducto = new producto (nombre, precio);
+                this.listaProductos.push(nuevoProducto);
+            }
+            this.escribirCarrito();
     }
 
     calcularTotal() {
@@ -30,20 +43,74 @@ class carrito {
     }
 
     escribirCarrito() {
+        console.log(this.listaProductos);
 
+        const tbody = document.getElementById("body")
+        tbody.innerHTML = "";
+        
+        for (let i = 0; i < this.listaProductos.length; i++) {
+            const prod = this.listaProductos[i];
+            const fila = document.createElement("tr");
+            
+            const nombreCelda = document.createElement("td");
+            nombreCelda.innerText = prod.nombre; // Asignación del nombre
+
+            const cantidadCelda = document.createElement("td");
+            cantidadCelda.innerText = prod.cantidad;
+            
+            const totalCelda = document.createElement("td");
+            totalCelda.innerText = prod.obtenerTotal().toFixed(2) + "€";
+
+            fila.appendChild(nombreCelda);
+            fila.appendChild(cantidadCelda);
+            fila.appendChild(totalCelda);
+
+            tbody.appendChild(fila);
+        }
+
+        // El total se pone al final, fuera del bucle
+        const celdaSI = document.getElementById("pie");
+        celdaSI.innerText = this.calcularTotal().toFixed(2) + "€";
     }
 }
 
-// Aquí se crean los productos y se les asigna un precio utilizando dataset
-document.getElementById("botonPan").dataset.nombre = "Pan";
-document.getElementById("botonPan").dataset.precio = 1.50;
+    /** * //Aquí se crean los productos y se les asigna un precio utilizando dataset
+    document.getElementById("botonPan").dataset.nombre = "Pan";
+    document.getElementById("botonPan").dataset.precio = 1.50;
 
-document.getElementById("botonLeche").dataset.nombre = "Leche";
-document.getElementById("botonLeche").dataset.precio = 0.99;
+    document.getElementById("botonLeche").dataset.nombre = "Leche";
+    document.getElementById("botonLeche").dataset.precio = 0.99;
 
-document.getElementById("botonHuevos").dataset.nombre = "Huevos";
-document.getElementById("botonHuevos").dataset.precio = 2.50;
+    document.getElementById("botonHuevos").dataset.nombre = "Huevos";
+    document.getElementById("botonHuevos").dataset.precio = 2.50;
 
-document.getElementById("botonManzana").dataset.nombre = "Manzana";
-document.getElementById("botonManzana").dataset.precio = 0.75;          
+    document.getElementById("botonManzana").dataset.nombre = "Manzana";
+    document.getElementById("botonManzana").dataset.precio = 0.75;   
+    **/   
+    
+    //CONFIGURACIÓN DEL DOM
 
+const miCarrito = new carrito();
+
+// La famosa array de arrays 🔥
+const datosProductos = [
+    ["Leche", 0.99],
+    ["Pan", 1.50],
+    ["Huevos", 2.50],
+    ["Manzana", 0.75]
+];
+
+const idsBotones = ["botonLeche", "botonPan", "botonHuevos", "botonManzana"];
+
+for (let i = 0; i < idsBotones.length; i++) {
+    const boton = document.getElementById(idsBotones[i]);
+
+    boton.dataset.nombre = datosProductos[i][0];
+    boton.dataset.precio = datosProductos[i][1];
+
+    boton.addEventListener('click', (e) => {
+        const nombre = e.target.dataset.nombre;
+        const precio = Number(e.target.dataset.precio);
+        miCarrito.agregarProducto(nombre, precio);
+    })
+}
